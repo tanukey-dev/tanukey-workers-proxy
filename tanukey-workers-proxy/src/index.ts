3ee097f9181019c2c219d675c315d35a72d43d49
tar_bin <tar.bin.master@gmail.com>
2024-10-22 11:27:12 +0900

    websocket proxyを修正

--- tanukey-workers-proxy/src/index.ts
+++ tanukey-workers-proxy/src/index.ts
@@ -1,66 +1,19 @@
 import { Hono, Context } from 'hono'
-import { cors } from 'hono/cors'
-import type { DurableObjectNamespace } from '@cloudflare/workers-types'
-
-type Env = {
-  Bindings: {
-    WEBSOCKET: DurableObjectNamespace
-  }
-}
-
-export class WebSocketConnection implements DurableObject {
-  private readonly sessions = new Set<WebSocket>()
-
-  async fetch(request: Request) {
-    if (request.headers.get('Upgrade') !== 'websocket') {
-      return new Response('Expected WebSocket', { status: 426 })
-    }
-
-    const pair = new WebSocketPair()
-    const [client, server] = Object.values(pair)
-
-    await this.handleSession(server as WebSocket)
-
-    return new Response(null, {
-      status: 101,
-      webSocket: client,
-    })
-  }
-
-  private async handleSession(webSocket: WebSocket): Promise<void> {
-    (webSocket as any).accept()
-    this.sessions.add(webSocket)
-
-    webSocket.addEventListener('message', async (event: MessageEvent) => {
-      this.sessions.forEach((session) => session.readyState === WebSocket.OPEN && session.send(event.data))
-    })
-
-    webSocket.addEventListener('close', async (event: CloseEvent) => {
-      this.sessions.delete(webSocket)
-    })
-  }
-}
 
 const proxy = async (c: Context, url: string) => {
   return fetch(new URL(c.req.path, url), c.req.raw);
 };
 
 const apiProxy = async (c: Context) => {
-  return proxy(c, apiServerPath);
+  console.log(c.var.API_HOST);
+  return proxy(c, `https://${c.env.API_HOST}`);
 };
 
 const frontProxy = async (c: Context) => {
-  return proxy(c, frontServerPath);
+  return proxy(c, `https://${c.env.FRONT_HOST}`);
 };
 
-const frontServerPath = 'https://front-staging.tarbin.net';
-const apiServerPath = 'https://api-staging.tarbin.net';
-
-const app = new Hono<Env>()
-
-app.use('*', cors({
-  origin: [frontServerPath, apiServerPath],
-}));
+const app = new Hono<{ Bindings: CloudflareBindings}>()
 
 app.all('/api/*', apiProxy)
 
@@ -103,16 +56,10 @@ app.get('/follows/*', apiProxy)
 
 // websocket
 // @ts-ignore
-app.get('/streaming*', (c) => {
-  const upgradeHeader = c.req.header('Upgrade')
-  if (upgradeHeader !== 'websocket') {
-    return c.text('Expected WebSocket', 426)
-  }
-
-  const id = c.env.WEBSOCKET.idFromName('websocket')
-  const connection = c.env.WEBSOCKET.get(id)
-  // @ts-ignore
-  return connection.fetch(c.req.raw)
+app.all('/streaming', (c) => {
+  const url = new URL(c.req.raw.url);
+  url.host = c.env.API_HOST;
+  return fetch(url, c.req.raw)
 })
 
 // frontend
